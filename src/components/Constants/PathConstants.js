7cc82cc878ffc1319ad8ec8f4ccd2fe00239eea2
SudhansuJana <Ssje890@gmail.com>
2022-02-27 23:16:54 +0530

    heroku

--- src/components/Constants/PathConstants.js
+++ src/components/Constants/PathConstants.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://anupam-covid-portal.herokuapp.com";
 
 export const MAP_COUNTRIES_URL = "https://disease.sh/v3/covid-19/countries";
